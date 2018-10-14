8b4580bcc0fa42df69bac5b99fc59f3fd7a040b5
MediatekInfo <ajscorp@list.ru>
2018-10-14 16:32:31 +0400

    GFH file types added

--- Source/Bootloader/mtktypes.h
+++ Source/Bootloader/mtktypes.h
@@ -10,6 +10,9 @@
 #define BL_EXIST_MAGIC  0x42424242
 
 /* MTK file types */
+#define GFH_HDR_MAGIC   0x004D4D4D
+#define GFH_GETMAGIC(m) ((m) & 0x00FFFFFF)
+#define MAGIC_MASK      0x00FFFFFF
 #define ARM_BL          0x0001                                                                      // 1st bootloader
 #define ARM_EXT_BL      0x0002                                                                      // 2nd bootloader
 
@@ -19,13 +22,75 @@
 /* MTK signature types */
 #define SIG_NONE        0
 #define SIG_PHASH       1                                                                           //SHA-1 hash
+#define MTK_FEND        0x454E4400                                                                  //"END" in reversed order
 
+typedef enum
+{
+    GFH_FILE_INFO          = 0x0000,
+    GFH_BL_INFO            = 0x0001,
+    GFH_ANTI_CLONE         = 0x0002,
+    GFH_BL_SEC_KEY         = 0x0003,
+    GFH_SCTRL_CERT         = 0x0004,
+    GFH_TOOL_AUTH          = 0x0005,
+    GFH_MTK_RESERVED1      = 0x0006,
+    GFH_BROM_CFG           = 0x0007,
+    GFH_BROM_SEC_CFG       = 0x0008,
+    GFH_MTK_RESERVED2      = 0x0009,
+    GFH_MTK_RESERVED3      = 0x000a,
+    GFH_ROOT_CERT          = 0x000b,
+    GFH_EXP_CHK            = 0x000c,
+    GFH_EPP_PARAM          = 0x000d,
+    GFH_CHIP_VER           = 0x000e,
+    GFH_MTK_RESERVED4      = 0x000f,
+    GFH_MD_SEC_CFG         = 0x0010,
+
+    GFH_EPP_INFO           = 0x0100,
+    GFH_EMI_LIST           = 0x0101,
+    GFH_CMEM_ID_INFO       = 0x0102,
+    GFH_CMEM_NOR_INFO      = 0x0103,
+    GFH_DSP_INFO           = 0x0104,
+
+    GFH_MAUI_INFO          = 0x0200,
+    GFH_MAUI_SEC           = 0x0201,
+    GFH_MAUI_CODE_KEY      = 0x0202,   //MAUI_SEC_KEY for code part
+    GFH_MAUI_SECURE_RO_KEY = 0x0203,   //MAUI_SEC_KEY for secure ro part
+    GFH_MAUI_RESOURCE_KEY  = 0x0204,   //MAUI_SEC_KEY for resource part
+
+    GFH_SECURE_RO_INFO     = 0x0205,
+
+    GFH_DL_PACKAGE_INFO    = 0x0206,
+    GFH_FLASH_INFO         = 0x0207,
+    GFH_MACR_INFO          = 0x0208,
+    GFH_ARM_BL_INFO        = 0x0209,
+    GFH_EMMC_BOOTING_INFO  = 0x020A,
+    GFH_FOTA_INFO          = 0x020B,
+    GFH_CBR_RECORD_INFO    = 0x020C,
+    GFH_CONFIDENTIAL_BIN_INFO = 0x020D,
+    GFH_CBR_INFO           = 0x020E,
+    GFH_MBA_INFO           = 0x020F,
+    GFH_BINARY_LOCATION    = 0x0210,
+
+    GFH_BOOT_CERT_CTRL_CONTENT = 0x0300,
+
+    GFH_TYPE_NUM,
+    GFH_TYPE_END           = (uint16_t)0xffff
+} GFH_TYPE;
+
+typedef struct
+{
+    uint32_t    H0;
+    uint32_t    H1;
+    uint32_t    H2;
+    uint32_t    H3;
+    uint32_t    H4;
+    uint32_t    Reserved[3];
+} MTK_PHASH, *pMTK_PHASH;
 
 typedef struct tag_GFH_Header
 {
     uint32_t    magic;
     uint16_t    size;
-    uint16_t    type;
+    GFH_TYPE    type;
 } GFH_Header;
 
 typedef struct tag_FILE_INFO_v1_
@@ -45,7 +110,7 @@ typedef struct tag_FILE_INFO_v1_
     uint32_t    sig_len;
     uint32_t    jump_offset;
     uint32_t    attr;
-} FILE_INFO_v1;
+} FILE_INFO_v1, *pFILE_INFO_v1;
 
 typedef struct
 {
