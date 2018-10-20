87515d6fc26df5533e317a9bbf2e0d4cd3b8f2d6
MediatekInfo <ajscorp@list.ru>
2018-10-20 13:11:47 +0400

    Bootloader and system projects are combined into one project.

--- Source/systemconfig.h
+++ Source/systemconfig.h
@@ -3,14 +3,6 @@
 
 #include "systypes.h"
 
-#define _DEBUG_             (1)
-#define _USEBATTERY_        (0)
-#define USEINTERRUPTS
-#define VIBRVoltage         VIBR_VO18V
-
-#define SystemMemorySize    (3 * 1024 * 1024)
-#define SysCacheSize        CACHE_32kB
-
 //Debug USART interface
 #define DBG_USARTn          0
 #define DBG_USARTINF        USARTINFO[DBG_USARTn]
@@ -20,6 +12,22 @@
 #define DBG_TXPIN           GPIO11
 #define DBG_TXPIN_MODE      GPIO11_MODE_U1TXD
 
+#if defined(TARGET_SYSTEM)
+
+#define _DEBUG_             (1)
+#define _USEBATTERY_        (0)
+#define USEINTERRUPTS
+#define VIBRVoltage         VIBR_VO18V
+
+#define SystemMemorySize    (3 * 1024 * 1024)
+#define SysCacheSize        CACHE_32kB
 #include "systemlib.h"
+
+#elif defined(TARGET_BOOTLOADER)
+
+#define _DEBUG_             (1)
+#include "mt6261.h"
+#include "debug.h"
+#endif
 
 #endif /* _SYSTEMCONFIG_H_ */
