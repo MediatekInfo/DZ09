266aa36eeb1e83c0e2731e69e70c01b1469b8495
Mediatekinfo <ajscorp@list.ru>
2018-12-28 16:14:55 +0400

    Macro fix

--- Source/Application/Drivers/appdrivers.h
+++ Source/Application/Drivers/appdrivers.h
@@ -6,20 +6,20 @@
 #endif
 #if defined(_ILI9341_LCD_DRIVER_)
 #include "ili9341.h"
-#define LCDDRV_Initialize()         ILI9341_Initialize()
-#define LCDDRV_Sleep()              ILI9341_SleepLCD()
-#define LCDDRV_Resume()             ILI9341_ResumeLCD()
-#define LCDDRV_SetOutputWindow()    ILI9341_SetOutputWindow()
+#define LCDDRV_Initialize()                 ILI9341_Initialize()
+#define LCDDRV_Sleep()                      ILI9341_SleepLCD()
+#define LCDDRV_Resume()                     ILI9341_ResumeLCD()
+#define LCDDRV_SetOutputWindow(a, b, c, d)  ILI9341_SetOutputWindow(a, b, c, d)
 #define _LCD_DRIVER_ASSIGNED_
 #endif
 #if defined(_NO_LCD_DRIVER_)
 #if defined(_LCD_DRIVER_ASSIGNED_)
 #error Duplicate LCD driver definition!
 #else
-#define LCDDRV_Initialize()         false
+#define LCDDRV_Initialize()                 false
 #define LCDDRV_Sleep()
 #define LCDDRV_Resume()
-#define LCDDRV_SetOutputWindow()    NULL
+#define LCDDRV_SetOutputWindow(a, b, c, d)  NULL
 #define _LCD_DRIVER_ASSIGNED_
 #endif
 #endif
