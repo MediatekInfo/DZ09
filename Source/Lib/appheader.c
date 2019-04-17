bb2b2481a54f699d54c2816543caed01c89ab2fb
Mediatekinfo <ajscorp@list.ru>
2019-04-17 12:03:01 +0400

    Duplicate definitions have been removed.

--- Source/Lib/appheader.c
+++ Source/Lib/appheader.c
@@ -1,38 +1,7 @@
 // This is an independent project of an individual developer. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <stdint.h>
-
-typedef struct tag_GFH_Header
-{
-    uint32_t    magic;
-    uint16_t    size;
-    uint16_t    type;
-} GFH_Header;
-
-typedef struct tag_FILE_INFO_v1_
-{
-    GFH_Header  hdr;
-    char        identifier[12];                                                                     // including '\0'
-    uint32_t    file_ver;
-
-    uint16_t    file_type;
-    uint8_t     flash_dev;
-    uint8_t     sig_type;
-
-    uint32_t    load_addr;
-    uint32_t    file_len;
-    uint32_t    max_size;
-    uint32_t    offset;
-    uint32_t    sig_len;
-    uint32_t    jump_offset;
-    uint32_t    attr;
-} FILE_INFO_v1;
-
-typedef struct tag_GFH_BL_INFO_v1
-{
-    GFH_Header  hdr;
-    uint32_t    bl_attr;
-} GFH_BL_INFO_v1;
+#include "mtktypes.h"
 
 typedef struct tag_APP_HEADER
 {
