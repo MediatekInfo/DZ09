a3349b62d9f7ecafdcb58a9601c7d4aa176a761f
Mediatekinfo <ajscorp@list.ru>
2019-12-20 14:25:38 +0400

    Fixed activation mode of vibro LDO.

--- Source/Lib/MT6261/Drivers/pmu.c
+++ Source/Lib/MT6261/Drivers/pmu.c
@@ -61,11 +61,11 @@ void PMU_SetVibrationOutput(boolean Enable)
     uint16_t En = (Enable) ? RG_VIBR_EN : 0;
 
 #if VIBRVoltage == VIBR_VO18V
-    VIBR_CON0 = RG_VIBR_VOSEL(VIBR_VO18V) | En;
+    VIBR_CON0 = RG_VIBR_VOSEL(VIBR_VO18V) | VIBR_ON_SEL | En;
 #elif VIBRVoltage == VIBR_VO28V
-    VIBR_CON0 = RG_VIBR_VOSEL(VIBR_VO28V) | En;
+    VIBR_CON0 = RG_VIBR_VOSEL(VIBR_VO28V) | VIBR_ON_SEL | En;
 #elif VIBRVoltage == VIBR_VO30V
-    VIBR_CON0 = RG_VIBR_VOSEL(VIBR_VO30V) | En;
+    VIBR_CON0 = RG_VIBR_VOSEL(VIBR_VO30V) | VIBR_ON_SEL | En;
 #endif
 #endif
 }
