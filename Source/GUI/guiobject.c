eb6b6e38445608ca8e272e294d5987eab10e9aa9
MediatekInfo <ajscorp@list.ru>
2022-01-19 11:43:05 +0400

    GUI_GetObjectRecursive() fixed.

--- Source/GUI/guiobject.c
+++ Source/GUI/guiobject.c
@@ -3,7 +3,7 @@
 /*
 * This file is part of the DZ09 project.
 *
-* Copyright (C) 2021 - 2019 AJScorp
+* Copyright (C) 2022 - 2019 AJScorp
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -146,7 +146,7 @@ static pGUIOBJECT GUI_GetObjectRecursive(pGUIOBJECT Parent, pPOINT pt)
             }
             return ResObject;
         }
-        tmpDLItem = DL_GetNextItem(tmpDLItem);
+        tmpDLItem = DL_GetPrevItem(tmpDLItem);
     }
     return NULL;
 }
