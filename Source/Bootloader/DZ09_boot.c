78c52af2e13090eaf483bff2180d22ba948a9ebe
MediatekInfo <ajscorp@list.ru>
2022-03-30 12:06:11 +0400

    swab32 moved to utils.h.

--- Source/Bootloader/DZ09_boot.c
+++ Source/Bootloader/DZ09_boot.c
@@ -24,12 +24,6 @@
 #include "utils.h"
 #include "dz09_boot.h"
 
-#define swab32(x) ((uint32_t)(\
-        (((uint32_t)(x) & (uint32_t)0x000000ffUL) << 24) |\
-        (((uint32_t)(x) & (uint32_t)0x0000ff00UL) <<  8) |\
-        (((uint32_t)(x) & (uint32_t)0x00ff0000UL) >>  8) |\
-        (((uint32_t)(x) & (uint32_t)0xff000000UL) >> 24)))
-
 extern uint32_t __ROMImageBase, __ROMImageLimit;
 
 void *BL_CheckFileByDescriptor(BL_Descr File)
